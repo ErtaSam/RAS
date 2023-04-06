c391219c3db95f474f23044996671bf51fa44080
Erta <ertasamuolyte@gmail.com>
2023-04-06 20:45:29 +0300

    Pradėta užduotis TDD būdu, parašyti fail'inantys testai.

---
+++ src/RAS.Core/Aggregates/Order/Entities/OrderEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Library.Interfaces;
+
+namespace RAS.Core.Aggregates.Order.Entities;
+
+public class OrderEntity : BaseEntity, IAggregateRoot
+{
+    public string Status { get; set; } = null!;
+    public decimal Sum { get; set; }
+
+    public void Cancelled()
+    {
+    }
+    public void Paid()
+    {
+    }
+    public void Approved()
+    {
+    }
+    public void BeingPrepared()
+    {
+    }
+    public void Prepared()
+    {
+    }
+}
+
---
+++ src/RAS.Tests/OrderTests.cs
@@ -0,0 +1,56 @@
+﻿using Moq;
+using RAS.Core.Aggregates.Order.Entities;
+using RAS.Core.Exceptions;
+using RAS.Core.Interfaces.User;
+using Utils.Library.Interfaces;
+
+namespace RAS.Tests;
+public class OrderTests
+{
+    [Fact]
+    public async Task OrderCancelled()
+    {
+        var order = new OrderEntity();
+
+        order.Cancelled();
+        Assert.True(order.Status == "Cancelled");
+    }
+
+    [Fact]
+    public async Task OrderPaid()
+    {
+        var order = new OrderEntity();
+
+        order.Cancelled();
+        Assert.True(order.Status == "Paid");
+    }
+
+    [Fact]
+    public async Task OrderApproved()
+    {
+        var order = new OrderEntity();
+
+        order.Cancelled();
+        Assert.True(order.Status == "Approved");
+    }
+
+    [Fact]
+    public async Task OrderBeingPrepared()
+    {
+        var order = new OrderEntity();
+
+        order.Cancelled();
+        Assert.True(order.Status == "BeingPrepared");
+    }
+
+    [Fact]
+    public async Task OrderPrepared()
+    {
+        var order = new OrderEntity();
+
+        order.Cancelled();
+        Assert.True(order.Status == "Prepared");
+    }
+
+
+}
