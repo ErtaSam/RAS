951a9beedcb6092de833441c3ab08b2b13ba3aec
Eitvydas Dzetaveckas <eitvydasas10@gmail.com>
2023-04-10 16:32:15 +0300

    Revert "Išmestas nereikalingas testas"
    This reverts commit db612b3958cf89a079b7f8c221557f1382b08220.

--- src/RAS.Mobile/src/app/app.component.spec.ts
+++ src/RAS.Mobile/src/app/app.component.spec.ts
@@ -21,4 +21,11 @@ describe('AppComponent', () => {
 		const app = fixture.componentInstance;
 		expect(app.title).toEqual('ras');
 	});
+
+	it('should render title', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		fixture.detectChanges();
+		const compiled = fixture.nativeElement as HTMLElement;
+		expect(compiled.querySelector('.content span')?.textContent).toContain('ras app is running!');
+	});
 });
