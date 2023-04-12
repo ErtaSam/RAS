5248686f25066a8df5a962aa147e0246a36bf62e
Eitvydas Dzetaveckas <eitvydasas10@gmail.com>
2023-04-12 20:44:10 +0300

    Fixed error on user token timeout

--- src/RAS.Mobile/src/app/features/auth/auth.routing.ts
+++ src/RAS.Mobile/src/app/features/auth/auth.routing.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OnlyUnauthorizedGuard } from '../../core/guards/only-unauthorized.guard';
 import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
@@ -14,6 +15,10 @@ const routes: Routes = [
 		path: 'register',
 		component: RegisterComponent,
 		canActivate: [OnlyUnauthorizedGuard],
+	},
+	{
+		path: 'logout',
+		component: LogoutComponent,
 	},
 ];
 
